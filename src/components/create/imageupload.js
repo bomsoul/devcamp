67a9323344aa113b8f65656df237468400bc33c2
Legoeiei <chattarin.par1@ku.th>
2019-07-03 11:28:44 +0700

    dragger upload

--- src/components/create/imageupload.js
+++ src/components/create/imageupload.js
@@ -1,5 +1,26 @@
 import React from 'react';
 import { storage } from '../../firebase';
+import ReactDOM from 'react-dom';
+import 'antd/dist/antd.css';
+import { Upload, Icon, message } from 'antd';
+
+const { Dragger } = Upload;
+const props = {
+    name: 'file',
+    multiple: true,
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    onChange(info) {
+        const { status } = info.file;
+        if (status !== 'uploading') {
+          console.log(info.file, info.fileList);
+        }
+        if (status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully.`);
+        } else if (status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+}
 export default class ImageUpload extends React.Component{
     constructor(props){
         super(props);
@@ -39,6 +60,17 @@ export default class ImageUpload extends React.Component{
         return(
             <div className="container" align="center">
             <progress value={this.state.progress} max="100"/>
+            <Dragger {...props}>
+                <p className="ant-upload-drag-icon">
+                    <Icon type="inbox" />
+                </p>
+                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                <p className="ant-upload-hint">
+                    Support for a single or bulk upload. Strictly prohibit from uploading company data or other
+                    band files
+                </p>
+            </Dragger>
+            <br></br>
                 <input type="file" onChange={this.handleChange}/>
                 <button onClick={this.handleUpload} className="btn btn-success">Upload</button>
             </div>
