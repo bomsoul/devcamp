ae6e523072117987bf799a932c49fc8ba57d9ae4
Teerayuth Artidakorn <bomzakup_123@hotmail.com>
2019-07-04 11:25:13 +0700

    image display

--- src/components/test.js
+++ src/components/test.js
@@ -2,24 +2,38 @@ import React from 'react';
 import firebase from 'firebase';
 
 const db=firebase.firestore();
+const image = '';
 export default class Test extends React.Component{
     constructor(props){
         super(props);
         this.state = { 
-            url: ''
+            url: []
         }
     }
 
+    componentDidMount(){
 
-    render(){
         db.collection('picture').get().then((snapshot)=>{
             snapshot.forEach(doc=>{
-                this.state.url = doc.data().urls;
-               console.log(this.state.url);
+                // this.state.url = doc.data().urls;
+                console.log(doc.data());
+                
+                this.setState({
+                    url: [ ...this.state.url , doc.data() ]
+                })
+                
             })
         })
+
+    }
+
+    render(){
+        console.log(this.state.url);
+        
         return(
-            <img src={this.props.url}/>
+            <div className="Test">
+                {this.state.url.map((u , index)=> <img key={index} src={u.urls} alt="hello my image" />)}
+            </div>
         )
     }
 }
